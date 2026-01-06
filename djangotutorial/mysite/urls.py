2c4945c4bbb2930df958e4933630b02f801734f0
Lucas-Dias-Estagiario <lucas.alves.estagiario@icmbio.gov.br>
2026-01-06 14:40:24 -0300

    estilizando os front-ends

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include  # <--- ADICIONE O ", include" AQUI
 from django.conf import settings
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path("admin/", admin.site.urls),
     path("polls/", include("polls.urls")),
 ]
